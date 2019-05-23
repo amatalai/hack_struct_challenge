a8d1ceb6e5a8cc7df73971a2a1e24e18b3a52aad
Tobiasz <tobiasz.malecki@appunite.com>
2019-05-23 23:02:15 +0200

    Hack __struct__ and defstruct

--- lib/struct_cop.ex
+++ lib/struct_cop.ex
@@ -1,2 +1,45 @@
 defmodule StructCop do
+  defmacro __using__(_opts) do
+    if struct_defined?(__CALLER__) do
+      handle_defined_struct()
+    else
+      handle_struct_to_be_defined()
+    end
+  end
+
+  defp struct_defined?(caller) do
+    !!Macro.Env.vars(caller)[:struct]
+  end
+
+  defp handle_defined_struct do
+    quote do
+      defoverridable __struct__: 1
+
+      def __struct__(kv) do
+        struct = super(kv)
+        validate(struct)
+      end
+    end
+  end
+
+  defp handle_struct_to_be_defined do
+    quote do
+      import Kernel, except: [defstruct: 1]
+      import StructCop, only: [defstruct: 1]
+    end
+  end
+
+  @doc false
+  defmacro defstruct(kv) do
+    quote do
+      Kernel.defstruct(unquote(kv))
+
+      defoverridable __struct__: 1
+
+      def __struct__(kv) do
+        struct = super(kv)
+        validate(struct)
+      end
+    end
+  end
 end
--- test/struct_cop_test.exs
+++ test/struct_cop_test.exs
@@ -1,2 +1,43 @@
 defmodule StructCopTest do
+  use ExUnit.Case, async: true
+
+  defmodule DefstructBeforeStructCop do
+    defstruct [:lulz]
+
+    use StructCop
+
+    def validate(struct) do
+      if struct.lulz, do: raise("for teh lulz!")
+
+      struct
+    end
+  end
+
+  defmodule DefstructAfterStructCop do
+    use StructCop
+
+    defstruct [:lulz]
+
+    def validate(struct) do
+      if struct.lulz, do: raise("for teh lulz!")
+
+      struct
+    end
+  end
+
+  test "__using__ can be invoked before defstruct" do
+    assert_raise(RuntimeError, "for teh lulz!", fn ->
+      ast = quote do: %DefstructBeforeStructCop{lulz: true}
+
+      Code.eval_quoted(ast, [], __ENV__)
+    end)
+  end
+
+  test "__using__ can be invoked after defstruct" do
+    assert_raise(RuntimeError, "for teh lulz!", fn ->
+      ast = quote do: %DefstructAfterStructCop{lulz: true}
+
+      Code.eval_quoted(ast, [], __ENV__)
+    end)
+  end
 end
